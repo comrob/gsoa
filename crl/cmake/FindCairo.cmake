b7880b409495586d2a6635d7e3d576aa8722c798
Jan Faigl <faiglj@fel.cvut.cz>
2022-11-26 15:12:38 +0100

    FIX: FindCairo.cmake

--- crl/cmake/FindCairo.cmake
+++ crl/cmake/FindCairo.cmake
@@ -1,64 +1,61 @@
-# - Try to find the cairo library
+# - try to find Cairo
 # Once done this will define
 #
-#  Cairo_FOUND - system has cairo
-#  Cairo_INCLUDE_DIRS - the cairo include directory
-#  Cairo_LIBRARIES - Link these to use cairo
+#  Cairo_FOUND - system has Cairo
+#  Cairo_CFLAGS - the Cairo CFlags
+#  Cairo_INCLUDE_DIRS - the Cairo include directories
+#  Cairo_LIBRARIES - Link these to use Cairo
 #
-# Define Cairo_MIN_VERSION for which version desired.
-#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-include(FindPkgConfig)
+if(Cairo_INCLUDE_DIRS AND Cairo_LIBRARIES)
 
-pkg_check_modules(PC_CAIRO cairo) 
+  # in cache already
+  set(CAIRO_FOUND TRUE)
 
-find_path(Cairo_INCLUDE_DIRS
-    NAMES cairo.h
-    HINTS ${PC_CAIRO_INCLUDEDIR}
-          ${PC_CAIRO_INCLUDE_DIRS}
-    PATH_SUFFIXES cairo
+else(Cairo_INCLUDE_DIRS AND Cairo_LIBRARIES)
+
+if(NOT WIN32)
+  # use pkg-config to get the directories and then use these values
+  # in the FIND_PATH() and FIND_LIBRARY() calls
+  find_package(PkgConfig REQUIRED)
+  if(Cairo_FIND_VERSION_COUNT GREATER 0)
+    set(_cairo_version_cmp ">=${Cairo_FIND_VERSION}")
+  endif(Cairo_FIND_VERSION_COUNT GREATER 0)
+  pkg_check_modules(_pc_cairo cairo${_cairo_version_cmp})
+  if(_pc_cairo_FOUND)
+    set(CAIRO_FOUND TRUE)
+  endif(_pc_cairo_FOUND)
+else(NOT WIN32)
+  # assume so, for now
+  set(CAIRO_FOUND TRUE)
+endif(NOT WIN32)
+
+if(CAIRO_FOUND)
+  # set it back as false
+  set(CAIRO_FOUND FALSE)
+
+  find_library(CAIRO_LIBRARY cairo
+               HINTS ${_pc_cairo_LIBRARY_DIRS}
+  )
+  set(Cairo_LIBRARIES "${CAIRO_LIBRARY}")
+
+  find_path(CAIRO_INCLUDE_DIR cairo.h
+            HINTS ${_pc_cairo_INCLUDE_DIRS}
+            PATH_SUFFIXES cairo
+  )
+  set(Cairo_INCLUDE_DIRS "${CAIRO_INCLUDE_DIR}")
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(Cairo DEFAULT_MSG Cairo_LIBRARIES Cairo_INCLUDE_DIRS)
+endif(CAIRO_FOUND)
+
+endif(Cairo_INCLUDE_DIRS AND Cairo_LIBRARIES)
+
+mark_as_advanced(
+  Cairo_CFLAGS
+  Cairo_INCLUDE_DIRS
+  Cairo_LIBRARIES
 )
 
-find_library(Cairo_LIBRARIES
-    NAMES cairo pthread
-    HINTS ${PC_CAIRO_LIBDIR}
-          ${PC_CAIRO_LIBRARY_DIRS}
-)
-
-IF (Cairo_INCLUDE_DIRS)
-    IF (EXISTS "${Cairo_INCLUDE_DIRS}/cairo-version.h")
-        FILE(READ "${Cairo_INCLUDE_DIRS}/cairo-version.h" CAIRO_VERSION_CONTENT)
-
-        STRING(REGEX MATCH "#define +CAIRO_VERSION_MAJOR +([0-9]+)" _dummy "${CAIRO_VERSION_CONTENT}")
-        SET(CAIRO_VERSION_MAJOR "${CMAKE_MATCH_1}")
-
-        STRING(REGEX MATCH "#define +CAIRO_VERSION_MINOR +([0-9]+)" _dummy "${CAIRO_VERSION_CONTENT}")
-        SET(CAIRO_VERSION_MINOR "${CMAKE_MATCH_1}")
-
-        STRING(REGEX MATCH "#define +CAIRO_VERSION_MICRO +([0-9]+)" _dummy "${CAIRO_VERSION_CONTENT}")
-        SET(CAIRO_VERSION_MICRO "${CMAKE_MATCH_1}")
-
-        SET(CAIRO_VERSION "${CAIRO_VERSION_MAJOR}.${CAIRO_VERSION_MINOR}.${CAIRO_VERSION_MICRO}")
-    ENDIF ()
-ENDIF ()
-
-# FIXME: Should not be needed anymore once we start depending on CMake 2.8.3
-SET(VERSION_OK TRUE)
-IF (Cairo_FIND_VERSION)
-    IF (Cairo_FIND_VERSION_EXACT)
-        IF ("${Cairo_FIND_VERSION}" VERSION_EQUAL "${CAIRO_VERSION}")
-            # FIXME: Use IF (NOT ...) with CMake 2.8.2+ to get rid of the ELSE block
-        ELSE ()
-            SET(VERSION_OK FALSE)
-        ENDIF ()
-    ELSE ()
-        IF ("${Cairo_FIND_VERSION}" VERSION_GREATER "${CAIRO_VERSION}")
-            SET(VERSION_OK FALSE)
-        ENDIF ()
-    ENDIF ()
-ENDIF ()
-
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Cairo DEFAULT_MSG Cairo_INCLUDE_DIRS Cairo_LIBRARIES VERSION_OK)
-
-mark_as_advanced(Cairo_LIBRARIES Cairo_INCLUDE_DIRS)
